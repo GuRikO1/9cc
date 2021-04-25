118d7634dd1dd9662d50bc4bc6aeefea3bcd02db
Yuriko Ezaki <e.guriko920@gmail.com>
2021-04-25 18:09:42 +0900

    create complier for one integer

---
+++ 9cc.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Wrong number of argument.\n");
+        return 1;
+    }
+    printf(".intel_syntax noprefix\n");
+    printf(".global main\n");
+    printf("main:\n");
+    printf("\tmov rax, %d\n", atoi(argv[1]));
+    printf("\tret\n");
+    return 0;
+}
