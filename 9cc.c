723bcbd3910db063061a44dd08f5e660d8662db5
Yuriko Ezaki <e.guriko920@gmail.com>
2021-05-17 02:17:27 +0900

    add addition and subtraction

--- 9cc.c
+++ 9cc.c
@@ -6,10 +6,29 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Wrong number of argument.\n");
         return 1;
     }
+    char *p = argv[1];
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("\tmov rax, %d\n", atoi(argv[1]));
+    printf("\tmov rax, %ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("\tadd rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("\tsub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "Unexpected character: %c\n", *p);
+        return 1;
+    }
+
     printf("\tret\n");
     return 0;
 }
